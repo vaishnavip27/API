ef41b50daccda06e53f6653c34a3e1bc75d34fe3
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-07-23 06:48:38 +0530

    commit

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
+  const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+  const dataObj = JSON.parse(data);
+
   if (pathName === "/" || pathName === "/overview") {
     res.end("This is the overview!");
   } else if (pathName === "/product") {
     res.end("This is the product");
   } else if (pathName === "/api") {
-    fs.readFile("./dev-data/data.json");
-
-    res.end("API");
-  } else {
-    res.writeHead(404, {
-      "Content-type": "/text/html",
-      "my-own-header": "hello-world",
+    res.writeHead(200, {
+      "Content-type": "application/json",
     });
+    res.end(data);
+  } else {
     res.end("Page not found!");
   }
 });
