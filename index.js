f927b4e8f628bbdf96c0f7c9a132cd5d3cb0c58a
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-07-24 10:31:23 +0530

    api

--- index.js
+++ index.js
@@ -4,18 +4,44 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
+  const tempOverview = fs.readFileSync(
+    `${__dirname}/templates/template-overview.html`,
+    "utf-8"
+  );
+  const tempCard = fs.readFileSync(
+    `${__dirname}/templates/template-card.html`,
+    "utf-8"
+  );
+  const tempProduct = fs.readFileSync(
+    `${__dirname}/templates/template-product.html`,
+    "utf-8"
+  );
+
   const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
   const dataObj = JSON.parse(data);
 
+  //Overview page
   if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the overview!");
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+
+    const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el));
+
+    res.end(tempOverview);
+
+    //Product page
   } else if (pathName === "/product") {
     res.end("This is the product");
+
+    //API
   } else if (pathName === "/api") {
     res.writeHead(200, {
       "Content-type": "application/json",
     });
     res.end(data);
+
+    //Not found
   } else {
     res.end("Page not found!");
   }
